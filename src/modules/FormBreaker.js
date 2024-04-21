1d8589586cf37d683ce945a8a152cc71a856c2a4
cihusss <cihusss@gmail.com>
2024-04-21 08:17:00 -0400

    dynamic group oppo and error handling

--- src/modules/FormBreaker.js
+++ src/modules/FormBreaker.js
@@ -1,54 +1,53 @@
 export const FormBreaker = (currentScript) => {
-
   const formId = document.getElementsByClassName('lp-form')[0].id.split('mktoForm_').pop();
   console.log('formId:', formId);
 
-  MktoForms2.loadForm("https://learn.bisk.com", "058-NIT-467", formId);
+  MktoForms2.loadForm('https://learn.bisk.com', '058-NIT-467', formId);
 
   //////////// Marketo Form Config ///////////
 
   const requiredFields = currentScript.getAttribute('requiredFields').split(',');
   const btnBg = currentScript.getAttribute('btnBg');
   const btnFg = currentScript.getAttribute('btnFg');
-  const rq = []
+  const groupField = currentScript.getAttribute('groupField');
+  const groupInputField = currentScript.getAttribute('groupInputField');
+  const rq = [];
 
   requiredFields.forEach((field) => {
     rq.push({
       name: field.trim(),
-      message: "This field is required."
+      message: 'This field is required.',
     });
   });
 
   var userConfig = {
     buttons: {
       prev: {
-        label: "Back",
-        disabled: false
+        label: 'Back',
+        disabled: false,
       },
       next: {
-        label: "Next"
-      }
+        label: 'Next',
+      },
     },
 
-    requiredFields: rq
-
+    requiredFields: rq,
   };
 
   //////////// Marketo Form Magic ///////////
 
-  MktoForms2.whenReady(function(form) {
-
+  MktoForms2.whenReady(function (form) {
     console.log('form is ready');
 
     let mForm = form;
     var formEl = form.getFormElem()[0],
       arrayify = getSelection.call.bind([].slice);
 
-    var fieldRowStor = ".mktoForm > .mktoFormRow",
-      buttonRowStor = ".mktoForm > .mktoButtonRow",
-      buttonStor = ".mktoButtonRow .mktoButton",
-      fsaatPrefix = "fsaat-",
-      localFragmentAttr = "data-form-local-fragment";
+    var fieldRowStor = '.mktoForm > .mktoFormRow',
+      buttonRowStor = '.mktoForm > .mktoButtonRow',
+      buttonStor = '.mktoButtonRow .mktoButton',
+      fsaatPrefix = 'fsaat-',
+      localFragmentAttr = 'data-form-local-fragment';
 
     var CSSOM_RULEPOS_FIRST = 0;
 
@@ -63,27 +62,24 @@ export const FormBreaker = (currentScript) => {
         return fieldDesc;
       })
       .forEach(function (fieldDesc) {
-        fieldDesc.label.parentNode.classList.add("mktoRequiredField");
+        fieldDesc.label.parentNode.classList.add('mktoRequiredField');
       });
 
-    var dynableSheet = arrayify(document.styleSheets)
-      .filter(function(sheet){
-        return sheet.ownerNode.nodeName == "STYLE";
-      })[0];
+    var dynableSheet = arrayify(document.styleSheets).filter(function (sheet) {
+      return sheet.ownerNode.nodeName == 'STYLE';
+    })[0];
 
-    arrayify(fieldRows).forEach(function(row, rowIdx) {
+    arrayify(fieldRows).forEach(function (row, rowIdx) {
       var rowPos = {
         isFirst: rowIdx == 0,
-        isLast: rowIdx == fieldRows.length - 1
+        isLast: rowIdx == fieldRows.length - 1,
       };
 
       // id each wrapper row in DOM order
 
       row.id = fsaatPrefix + rowIdx;
 
-      var navButtonRow = rowPos.isLast
-          ? submitButtonRow
-          : submitButtonRow.cloneNode(true),
+      var navButtonRow = rowPos.isLast ? submitButtonRow : submitButtonRow.cloneNode(true),
         newRowAxis = row.nextSibling,
         nextEnabled = !rowPos.isLast,
         prevEnabled = !rowPos.isFirst && !userConfig.buttons.prev.disabled,
@@ -100,9 +96,9 @@ export const FormBreaker = (currentScript) => {
         navButtons.prev = newButtonTmpl.cloneNode();
       }
 
-      Object.keys(navButtons).forEach(function(dir) {
-        navButtons[dir].type = "button";
-        navButtons[dir].setAttribute("data-dir", dir);
+      Object.keys(navButtons).forEach(function (dir) {
+        navButtons[dir].type = 'button';
+        navButtons[dir].setAttribute('data-dir', dir);
         navButtons[dir].innerHTML = userConfig.buttons[dir].label;
       });
 
@@ -114,28 +110,45 @@ export const FormBreaker = (currentScript) => {
         newButtonAxis.parentNode.insertBefore(navButtons.prev, newButtonAxis);
       }
 
-      navButtonRow.addEventListener("click", function(e) {
-        if (e.target.tagName == "BUTTON" && e.target.type == "button" ) {
-          if (e.target.getAttribute("data-dir") == "next" && !isCustomValid(true,row)) {
+      navButtonRow.addEventListener('click', function (e) {
+        if (e.target.tagName == 'BUTTON' && e.target.type == 'button') {
+          if (e.target.getAttribute('data-dir') == 'next' && !isCustomValid(true, row)) {
             return;
           }
-          fsaatSet(row, e.target.getAttribute("data-dir"));
+          fsaatSet(row, e.target.getAttribute('data-dir'));
         }
       });
 
       dynableSheet.insertRule(
         [
-          ".mktoForm[" + localFragmentAttr + "='#" + row.id + "']" + " " + ".mktoFormRow#" + row.id + ",",
-          ".mktoForm[" + localFragmentAttr + "='#" + row.id + "']" + " " + ".mktoFormRow#" + row.id + " + " + ".mktoButtonRow",
-          "{ display: block; }"
-        ].join(" "),
+          '.mktoForm[' +
+            localFragmentAttr +
+            "='#" +
+            row.id +
+            "']" +
+            ' ' +
+            '.mktoFormRow#' +
+            row.id +
+            ',',
+          '.mktoForm[' +
+            localFragmentAttr +
+            "='#" +
+            row.id +
+            "']" +
+            ' ' +
+            '.mktoFormRow#' +
+            row.id +
+            ' + ' +
+            '.mktoButtonRow',
+          '{ display: block; }',
+        ].join(' '),
         CSSOM_RULEPOS_FIRST
       );
     });
 
     function fsaatSet(current, dir) {
-      var FSAAT_DIR_PREV = "prev",
-        FSAAT_DIR_NEXT = "next";
+      var FSAAT_DIR_PREV = 'prev',
+        FSAAT_DIR_NEXT = 'next';
 
       var dir = dir || FSAAT_DIR_NEXT,
         currentIndex,
@@ -149,24 +162,27 @@ export const FormBreaker = (currentScript) => {
         currentIndex = -1;
       }
 
-      newHash = "#" + fsaatPrefix + (dir == FSAAT_DIR_NEXT ? ++currentIndex : --currentIndex);
+      newHash = '#' + fsaatPrefix + (dir == FSAAT_DIR_NEXT ? ++currentIndex : --currentIndex);
 
       formEl.setAttribute(localFragmentAttr, newHash);
     }
 
-    function isCustomValid(native, currentStep){
+    function isCustomValid(native, currentStep) {
       form.submittable(false);
 
       var currentStep = currentStep || formEl,
         currentValues = form.getValues();
 
-      var currentUnfilled = userConfig.requiredFields
-        .filter(function(fieldDesc) {
-          return currentStep.contains(fieldDesc.refEl) && (!currentValues[fieldDesc.name] || (fieldDesc.refEl.type == "checkbox" && currentValues[fieldDesc.name] == "no"));
-        });
+      var currentUnfilled = userConfig.requiredFields.filter(function (fieldDesc) {
+        return (
+          currentStep.contains(fieldDesc.refEl) &&
+          (!currentValues[fieldDesc.name] ||
+            (fieldDesc.refEl.type == 'checkbox' && currentValues[fieldDesc.name] == 'no'))
+        );
+      });
 
       if (currentUnfilled.length) {
-        form.showErrorMessage(currentUnfilled[0].message,MktoForms2.$(currentUnfilled[0].refEl));
+        form.showErrorMessage(currentUnfilled[0].message, MktoForms2.$(currentUnfilled[0].refEl));
         return false;
       } else {
         form.submittable(true);
@@ -186,13 +202,13 @@ export const FormBreaker = (currentScript) => {
     for (i = 0; i < labels.length; i++) {
       let label = document.getElementsByClassName('mktoLabel')[i];
       label.style.opacity = '0';
-      name = label.innerHTML.split("</div>").pop();
+      name = label.innerHTML.split('</div>').pop();
       let input = label.nextSibling.nextSibling;
       input.setAttribute('placeholder', name);
     }
 
     const labelLogic = (input) => {
-      if (input.type === "text") {
+      if (input.type === 'text') {
         input.addEventListener('focusin', function () {
           let boo = this.previousSibling.previousSibling;
           boo.style.opacity = '1';
@@ -201,37 +217,49 @@ export const FormBreaker = (currentScript) => {
         });
         input.addEventListener('focusout', function () {
           let boo = this.previousSibling.previousSibling;
-          let placeholder = this.setAttribute('placeholder', boo.innerHTML.split("</div>").pop());
-          if (this.value == "") {
+          let placeholder = this.setAttribute('placeholder', boo.innerHTML.split('</div>').pop());
+          if (this.value == '') {
             boo.style.opacity = '0';
             this.setAttribute('style', 'padding: 2px 6px 0 6px !important');
           } else {
             boo.style.opacity = '1';
           }
         });
+      } else if (input.type === 'radio') {
       }
-      else if (input.type === "radio") {
-      }
-    }
+    };
 
     for (i = 0; i < inputs.length; i++) {
       let input = document.getElementsByClassName('mktoField')[i];
       labelLogic(input);
     }
 
-   document.querySelectorAll('input[name="Group_Opportunity__c"]')[1].addEventListener('click', () => {
-     console.log('clicked');
-     setTimeout(() => {
-       let input = document.querySelectorAll('input[name="Company_Name__c"]')[0];
-       input.placeholder = document.querySelectorAll('label[for="Company_Name__c"]')[0].innerText;
-       labelLogic(input);
-     }, 10);
-   });
+    if (
+      groupField !== undefined &&
+      groupInputField !== undefined &&
+      groupField !== '' &&
+      groupInputField !== ''
+    ) {
+      try {
+        document.querySelectorAll(`input[name=${groupField}]`)[1].addEventListener('click', () => {
+          console.log('clicked');
+          setTimeout(() => {
+            let input = document.querySelectorAll(`input[name=${groupInputField}]`)[0];
+            input.placeholder = document.querySelectorAll(
+              `label[for=${groupInputField}]`
+            )[0].innerText;
+            labelLogic(input);
+          }, 10);
+        });
+      } catch (e) {
+        console.log('no group field found');
+      }
+    }
 
     console.log(inputs[0].value);
 
     let btnSubmit = document.querySelector('button[type="submit"]');
-    btnSubmit.addEventListener('click', function() {
+    btnSubmit.addEventListener('click', function () {
       for (i = 0; i < inputs.length; i++) {
         let inputVal = document.getElementsByClassName('mktoField')[i].value;
         console.log(inputVal);
@@ -240,11 +268,11 @@ export const FormBreaker = (currentScript) => {
       // console.log(inputValues);
       if (inputValues.includes('')) {
         console.log('one of the values is empty');
-      }
-      else {
+      } else {
         btnSubmit.innerHTML = 'Wait...';
         // alert('booooom');
-        document.querySelector('button[type="button"]:nth-child(1)').style.display = 'none !important';
+        document.querySelector('button[type="button"]:nth-child(1)').style.display =
+          'none !important';
       }
     });
     btnSubmit.style.background = btnBg;
@@ -255,7 +283,7 @@ export const FormBreaker = (currentScript) => {
     btnNext.style.color = btnFg;
 
     let legends = document.querySelectorAll('legend');
-    legends.forEach(element => {
+    legends.forEach((element) => {
       let currText = element.innerHTML.replace('0', '');
       let steps = document.querySelectorAll('fieldset');
       element.innerHTML = currText + ' of ' + steps.length;
@@ -263,12 +291,12 @@ export const FormBreaker = (currentScript) => {
     });
 
     let lala = document.querySelectorAll('.mktoField');
-    lala.forEach(element => {
-      element.addEventListener('focusin', function() {
+    lala.forEach((element) => {
+      element.addEventListener('focusin', function () {
         // console.log('focusIn');
         // element.setAttribute('style', 'padding: 12px 6px 0 6px !important');
       });
-      element.addEventListener('focusout', function() {
+      element.addEventListener('focusout', function () {
         // console.log('focusOut');
         // element.setAttribute('style', 'padding: 2px 6px 0 6px !important');
       });
@@ -276,45 +304,42 @@ export const FormBreaker = (currentScript) => {
 
     // Group Dropdowns
 
-    arrayify(formEl.querySelectorAll('SELECT'))
-      .forEach(function(selectEl) {
-        var currentOg;
-        arrayify(selectEl.querySelectorAll('OPTION'))
-          .forEach(function(o, idx) {
-            if (o.value == '[OPTGROUP]') {
-              currentOg = document.createElement('OPTGROUP');
-              currentOg.label = o.textContent;
-              selectEl.appendChild(currentOg);
-              selectEl.removeChild(o);
-            } else {
-              if (currentOg) currentOg.appendChild(o);
-            }
-          });
+    arrayify(formEl.querySelectorAll('SELECT')).forEach(function (selectEl) {
+      var currentOg;
+      arrayify(selectEl.querySelectorAll('OPTION')).forEach(function (o, idx) {
+        if (o.value == '[OPTGROUP]') {
+          currentOg = document.createElement('OPTGROUP');
+          currentOg.label = o.textContent;
+          selectEl.appendChild(currentOg);
+          selectEl.removeChild(o);
+        } else {
+          if (currentOg) currentOg.appendChild(o);
+        }
       });
+    });
 
     // Disable Inputs
 
     let hiddenEls = formEl.querySelectorAll('.mktoFormRow input[type="hidden"]');
-    for(var i = 0; i < hiddenEls.length; i++) {
-      hiddenEls[i].parentNode.style.display = "none";
+    for (var i = 0; i < hiddenEls.length; i++) {
+      hiddenEls[i].parentNode.style.display = 'none';
     }
 
     let optionEls = formEl.querySelectorAll('OPTION'),
       disabledTag = /^\[DISABLED\]\s/,
       _forEach = Array.prototype.forEach;
-    _forEach.call(optionEls,function(opt){
+    _forEach.call(optionEls, function (opt) {
       if (disabledTag.test(opt.textContent)) {
-        opt.textContent = opt.textContent.replace(disabledTag,'');
+        opt.textContent = opt.textContent.replace(disabledTag, '');
         opt.disabled = true;
       }
     });
 
     // Add hidden fields
-    form.addHiddenFields({ Landing_Page_URL__c : document.location} );
+    form.addHiddenFields({ Landing_Page_URL__c: document.location });
     // form.addHiddenFields({ mktoUTMTerm : currHead });
 
     // Show Form
     //document.getElementById("lp-form").style.opacity = 1;
-
   });
-}
+};
